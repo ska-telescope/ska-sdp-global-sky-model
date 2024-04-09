03e5de81b59f09595c463dfc6e83706b43cb5aac
Jacob Maresca <jacob.maresca@akkodis.com>
2024-04-09 13:04:37 +0100

    NAL-1008 update tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -7,13 +7,11 @@ from fastapi.testclient import TestClient
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-from ska_sdp_global_sky_model.kubernetes.api.app.main import Base, app, get_db
+from ska_sdp_global_sky_model.api.app.main import Base, app, get_db
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
+SQLALCHEMY_DATABASE_URL = "postgresql:///./test.db"
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 TestingSessionLocal = sessionmaker(
     autocommit=False, autoflush=False, bind=engine
 )
