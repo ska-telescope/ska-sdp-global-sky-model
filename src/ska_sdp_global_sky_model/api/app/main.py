5478510a014b61030753ca5cec3a29e89fcd8fa4
Christian Christelis <cchristelis@ska.ac.za>
2024-04-29 11:29:36 +0200

    NAL-1027 Added intial model development.

--- src/ska_sdp_global_sky_model/api/app/main.py
+++ src/ska_sdp_global_sky_model/api/app/main.py
@@ -3,17 +3,15 @@ A simple fastAPI.
 """
 
 from fastapi import Depends, FastAPI
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import Session, sessionmaker
+from sqlalchemy.orm import Session
 from starlette.middleware.cors import CORSMiddleware
+from astropy.coordinates import SkyCoord
+import astropy.units as u
 
 from ska_sdp_global_sky_model.api.app import crud
-from ska_sdp_global_sky_model.api.app.config import DB_URL
 
-engine = create_engine(DB_URL)
-session_local = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base = declarative_base()
+from ska_sdp_global_sky_model.api.app.model import PointSource
+from ska_sdp_global_sky_model.api.app.config import session_local, engine, Base
 
 app = FastAPI()
 
@@ -61,3 +59,17 @@ def test(db: Session = Depends(get_db)):
     Requests version information from pg_sphere.
     """
     return crud.get_pg_sphere_version(db=db)
+
+
+@app.get("/point-source-create", summary="Create a point source for testing")
+def point_source(db: Session = Depends(get_db)):
+    c3 = SkyCoord(1 * u.deg, 1 * u.deg)
+    source = PointSource(name='TEST', point=c3)
+    db.add(source)
+    db.commit()
+
+
+@app.get("/view-sources", summary="See all the point sources")
+def get_point_sources(db: Session = Depends(get_db)):
+    sources = db.query(PointSource).all()
+    return sources
