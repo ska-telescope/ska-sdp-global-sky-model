c25d906915f2f7b99128aa5ea8048943489e05fc
Christian Christelis <cchristelis@ska.ac.za>
2024-04-30 13:45:38 +0200

    NAL-1027 Updated models to include full spectrum data.

--- src/ska_sdp_global_sky_model/api/app/model.py
+++ src/ska_sdp_global_sky_model/api/app/model.py
@@ -12,6 +12,10 @@ class PointSource(Base):
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, unique=True)
     point = Column(Point)
+    A_Wide = Column(Float)
+    A_Wide_Error = Column(Float)
+    B_Wide = Column(Float)
+    B_Wide_Error = Column(Float)
 
 
 class Telescope(Base):
@@ -20,7 +24,8 @@ class Telescope(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, unique=True)
-
+    frequency_min = Column(Float)
+    frequency_max = Column(Float)
 
 class Band(Base):
     """Model the bands that the sources were observed in"""
@@ -39,8 +44,27 @@ class Spectral(Base):
     rms_noise = Column(Float)
     flux = Column(Float)
     source = relationship("PointSource", back_populates="spectral")
-    band = relationship("PointSource", back_populates="spectral")
+    band = relationship("PointSource")
 
 
-# class SpectralWide(Base):
-#     """Band wide spectral data"""
+class Spectrum(Base):
+    """Full Spectral band wide data"""
+    _tablename__ = "spectrum"
+    id = Column(Integer, primary_key=True, index=True)
+    Bck_Wide = Column(Float)
+    Local_RMS_Wide = Column(Float)
+    Flux_Wide = Column(Float)
+    Flux_Wide_Error = Column(Float)
+    Int_Flux_Wide = Column(Float)
+    Int_Flux_Wide_Error = Column(Float)
+    PA_Wide = Column(Float)
+    PA_Wide_Error = Column(Float)
+    Resid_Mean_Wide = Column(Float)
+    Resid_Sd_Wide = Column(Float)
+    Abs_Flux_Pct_Error = Column(Float)
+    Fit_Flux_Pct_Error = Column(Float)
+    A_PSF_Wide = Column(Float)
+    B_PSF_Wide = Column(Float)
+    PA_PSF_Wide = Column(Float)
+    telescope = relationship("Telescope")
+    source = relationship("PointSource", back_populates="spectrum")
