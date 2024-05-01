48dee72cd4af40f81c6ff88709e249a582b2f24c
Jacob Maresca <jacob.maresca@akkodis.com>
2024-05-01 10:31:00 +0100

    NAL-1027 move configs out of main

--- src/ska_sdp_global_sky_model/api/app/config.py
+++ src/ska_sdp_global_sky_model/api/app/config.py
@@ -5,6 +5,9 @@ Configure variables to be used.
 from pathlib import Path
 
 from starlette.config import Config
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
 ENV_FILE = Path(".env")
 if not ENV_FILE.exists():
@@ -26,3 +29,8 @@ SESSION_DB_NAME: int = config("SESSION_DB_NAME", default=0)
 SESSION_DB_HOST: str = config("SESSION_DB_HOST", default="session-db")
 SESSION_DB_PORT: int = config("SESSION_DB_PORT", default=6379)
 SESSION_DB_TOKEN_KEY: str = config("SESSION_DB_TOKEN_KEY", default="secret")
+
+
+engine = create_engine(DB_URL)
+session_local = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
