3452fd760e437bde9d3583226d82981616fe0c9f
Jacob Maresca <jacob.maresca@akkodis.com>
2024-05-01 12:50:16 +0100

    NAL-1027 updates to models

--- src/ska_sdp_global_sky_model/api/app/model.py
+++ src/ska_sdp_global_sky_model/api/app/model.py
@@ -12,12 +12,19 @@ from ska_sdp_global_sky_model.api.app.config import Base
 
 
 class Source(Base):
-    """Model representing sources and their location"""
+    """
+    Model representing sources and their location.
+    Derived from the wide (170-231MHz) image
+    """
 
     __tablename__ = "sources"
 
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, unique=True)
+    RAJ2000 = Column(Float)
+    RAJ2000_Error = Column(Float)
+    DecJ2000 = Column(Float)
+    DecJ2000_Error = Column(Float)
     Heal_Pix_Position = Column(Point)
     A_Wide = Column(Float)
     A_Wide_Error = Column(Float)
@@ -25,6 +32,8 @@ class Source(Base):
     B_Wide_Error = Column(Float)
     PA_Wide = Column(Float)
     PA_Wide_Error = Column(Float)
+    Flux_Wide = Column(Float)
+    Flux_Wide_Error = Column(Float)
 
 
 class Telescope(Base):
@@ -67,12 +76,8 @@ class Spectrum(Base):
     id = Column(Integer, primary_key=True, index=True)
     Bck_Wide = Column(Float)
     Local_RMS_Wide = Column(Float)
-    Flux_Wide = Column(Float)
-    Flux_Wide_Error = Column(Float)
     Int_Flux_Wide = Column(Float)
     Int_Flux_Wide_Error = Column(Float)
-    PA_Wide = Column(Float)
-    PA_Wide_Error = Column(Float)
     Resid_Mean_Wide = Column(Float)
     Resid_Sd_Wide = Column(Float)
     Abs_Flux_Pct_Error = Column(Float)
@@ -80,5 +85,7 @@ class Spectrum(Base):
     A_PSF_Wide = Column(Float)
     B_PSF_Wide = Column(Float)
     PA_PSF_Wide = Column(Float)
+    Spectral_Index = Column(Float)
+    Spectral_Index_Error = Column(Float)
     telescope = relationship("Telescope")
     source = relationship("Source", back_populates="spectrum")
