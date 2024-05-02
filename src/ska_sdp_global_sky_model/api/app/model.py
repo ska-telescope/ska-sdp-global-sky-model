574f95f62cd4b2a1c027308e4146286bbc9ebde5
Jacob Maresca <jacob.maresca@akkodis.com>
2024-05-02 14:28:32 +0100

    NAL-1027 update schema

--- src/ska_sdp_global_sky_model/api/app/model.py
+++ src/ska_sdp_global_sky_model/api/app/model.py
@@ -19,21 +19,13 @@ class Source(Base):
 
     __tablename__ = "sources"
 
-    id = Column(Integer, primary_key=True, index=True)
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
     name = Column(String, unique=True)
     RAJ2000 = Column(Float)
     RAJ2000_Error = Column(Float)
     DecJ2000 = Column(Float)
     DecJ2000_Error = Column(Float)
     Heal_Pix_Position = Column(Point)
-    A_Wide = Column(Float)
-    A_Wide_Error = Column(Float)
-    B_Wide = Column(Float)
-    B_Wide_Error = Column(Float)
-    PA_Wide = Column(Float)
-    PA_Wide_Error = Column(Float)
-    Flux_Wide = Column(Float)
-    Flux_Wide_Error = Column(Float)
 
 
 class Telescope(Base):
@@ -41,7 +33,7 @@ class Telescope(Base):
 
     __tablename__ = "telescope"
 
-    id = Column(Integer, primary_key=True, index=True)
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
     name = Column(String, unique=True)
     frequency_min = Column(Float)
     frequency_max = Column(Float)
@@ -51,29 +43,46 @@ class Band(Base):
     """Model the bands that the sources were observed in"""
 
     __band__ = "band"
-    id = Column(Integer, primary_key=True, index=True)
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
     centre = Column(Float)
     width = Column(Float)
     telescope = relationship("Telescope", back_populates="band")
 
 
-class Spectral(Base):
+class NarrowBandData(Base):
     """The observed spectral information"""
 
-    _tablename__ = "spectral"
-    id = Column(Integer, primary_key=True, index=True)
-    background = Column(Float)
-    rms_noise = Column(Float)
-    flux = Column(Float)
-    source = relationship("Source", back_populates="spectral")
+    _tablename__ = "NarrowBandData"
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    Bck_Narrow = Column(Float)
+    Local_RMS_Narrow = Column(Float)
+    Int_Flux_Narrow = Column(Float)
+    Int_Flux_Narrow_Error = Column(Float)
+    Resid_Mean_Narrow = Column(Float)
+    Resid_Sd_Narrow = Column(Float)
+
+    A_PSF_Narrow = Column(Float)
+    B_PSF_Narrow = Column(Float)
+    PA_PSF_Narrow = Column(Float)
+
+    A_Narrow = Column(Float)
+    A_Narrow_Error = Column(Float)
+    B_Narrow = Column(Float)
+    B_Narrow_Error = Column(Float)
+    PA_Narrow = Column(Float)
+    PA_Narrow_Error = Column(Float)
+    Flux_Narrow = Column(Float)
+    Flux_Narrow_Error = Column(Float)
+
+    source = relationship("Source", back_populates="NarrowBandData")
     band = relationship("Source")
 
 
-class Spectrum(Base):
+class WideBandData(Base):
     """Full Spectral band wide data"""
 
-    _tablename__ = "spectrum"
-    id = Column(Integer, primary_key=True, index=True)
+    _tablename__ = "WideBandData"
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
     Bck_Wide = Column(Float)
     Local_RMS_Wide = Column(Float)
     Int_Flux_Wide = Column(Float)
@@ -82,10 +91,22 @@ class Spectrum(Base):
     Resid_Sd_Wide = Column(Float)
     Abs_Flux_Pct_Error = Column(Float)
     Fit_Flux_Pct_Error = Column(Float)
+
     A_PSF_Wide = Column(Float)
     B_PSF_Wide = Column(Float)
     PA_PSF_Wide = Column(Float)
+
     Spectral_Index = Column(Float)
     Spectral_Index_Error = Column(Float)
+
+    A_Wide = Column(Float)
+    A_Wide_Error = Column(Float)
+    B_Wide = Column(Float)
+    B_Wide_Error = Column(Float)
+    PA_Wide = Column(Float)
+    PA_Wide_Error = Column(Float)
+    Flux_Wide = Column(Float)
+    Flux_Wide_Error = Column(Float)
+
     telescope = relationship("Telescope")
-    source = relationship("Source", back_populates="spectrum")
+    source = relationship("Source", back_populates="WideBandData")
