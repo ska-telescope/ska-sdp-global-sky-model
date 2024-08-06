d17af7688b05e331cccc59f9bc0a6e3aa8edc875
Jacob Maresca <jacob.maresca@gmail.com>
2024-08-06 08:05:56 +0100

    NAL-1136 data access methods

---
+++ src/ska_sdp_global_sky_model/api/app/__init__.py
@@ -0,0 +1,7 @@
+"""
+Data access methods.
+"""
+
+from .model import Band, Field, FieldTile, NarrowBandData, Source, Telescope, WideBandData
+
+__all__ = ["Band", "Field", "FieldTile", "NarrowBandData", "Source", "Telescope", "WideBandData"]
