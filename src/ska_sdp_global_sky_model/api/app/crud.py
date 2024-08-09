d1a4167cf3acc1899b80528000f27d0e40751433
Jacob Maresca <jacob.maresca@gmail.com>
2024-08-09 18:35:54 +0100

    NAL-1124 add alternate sky model endpoint

--- src/ska_sdp_global_sky_model/api/app/crud.py
+++ src/ska_sdp_global_sky_model/api/app/crud.py
@@ -6,14 +6,16 @@ CRUD functionality goes here.
 import logging
 
 import astropy.units as u
-from astropy.coordinates import Latitude, Longitude
+from astropy.coordinates import Latitude, Longitude, SkyCoord
+from astropy_healpix import HEALPix
 from cdshealpix import cone_search
 from healpix_alchemy import Tile
 from mocpy import MOC
 from sqlalchemy import and_
 from sqlalchemy.orm import Session
 
-from ska_sdp_global_sky_model.api.app.model import Field, FieldTile, Source
+from ska_sdp_global_sky_model.api.app.model import Field, FieldTile, SkyTile, Source
+from ska_sdp_global_sky_model.configuration.config import NSIDE
 
 logger = logging.getLogger(__name__)
 
@@ -94,6 +96,75 @@ def get_local_sky_model(
     # A Source is within the region of interest if a constituent FieldTile contains
     # the Source's HEALPix index
     query = db.query(Source).filter(FieldTile.hpx.contains(Source.Heal_Pix_Position)).all()
+
+    logger.info(
+        "Retrieve %s point sources within the area of interest.",
+        str(len(query)),
+    )
+
+    local_sky_model = {
+        "region": {"ra": ra, "dec": dec},
+        "count": len(query),
+        "sources_in_area_of_interest": [source.to_json(db) for source in query],
+    }
+
+    return local_sky_model
+
+
+def alternate_local_sky_model(
+    db,
+    ra: list,
+    dec: list,
+    _flux_wide: float,
+    _telescope: str,
+    fov: float,
+) -> dict:
+    """
+    Retrieves a local sky model (LSM) from a global sky model for a specific celestial observation.
+
+    The LSM contains information about celestial sources within a designated region of the sky. \
+        This function extracts this information from a database (`db`) based on the provided \
+        right ascension (RA) and declination (Dec) coordinates.
+
+    Args:
+        db (Any): A database object containing the global sky model. The specific type of \
+            database object will depend on the implementation.
+        ra (list[float]): A list containing two right ascension values (in degrees) that define \
+            the boundaries of the desired LSM region.
+        dec (list[float]): A list containing two declination values (in degrees) that define the \
+            boundaries of the desired LSM region.
+        _flux_wide (float): Placeholder for future implementation of wide-field flux \
+            of the observation (in Jy). Currently not used.
+        _telescope (str): Placeholder for future implementation of the telescope name \
+            being used for the observation. Currently not used.
+        _fov (float): Placeholder for future implementation of the telescope's field of\
+            view (in arcminutes). Currently not used.
+
+    Returns:
+        dict: A dictionary containing the LSM data. The structure of the dictionary is:
+
+            {
+                "region": {
+                    "ra": List of RA coordinates (same as input `ra`).
+                    "dec": List of Dec coordinates (same as input `dec`).
+                },
+                "count": Number of celestial sources found within the LSM region.
+                "sources_in_area_of_interest": List of dictionaries, each representing a \
+                    celestial source within the LSM region.
+                    The structure of each source dictionary depends on the data model stored \
+                        in the database (`db`).
+            }
+    """
+
+    hp = HEALPix(nside=NSIDE, order="nested", frame="icrs")
+    coord = SkyCoord(
+        Longitude(float(ra[0]) * u.deg), Latitude(float(dec[0]) * u.deg), frame="icrs"
+    )
+    tiles = hp.cone_search_skycoord(coord, radius=float(fov) * u.deg)
+    tiles += 4 * NSIDE**2
+    tiles_int = getattr(tiles, "tolist", lambda: tiles)()
+
+    query = db.query(Source).join(SkyTile).filter(SkyTile.pk.in_(tiles_int)).all()
 
     logger.info(
         "Retrieve %s point sources within the area of interest.",
