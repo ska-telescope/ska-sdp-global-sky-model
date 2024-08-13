597da7bb9746590bdb32f2ca7878528587427c74
Jacob Maresca <jacob.maresca@gmail.com>
2024-08-13 11:14:43 +0100

    NAL-1124 add third option

--- src/ska_sdp_global_sky_model/api/app/main.py
+++ src/ska_sdp_global_sky_model/api/app/main.py
@@ -18,6 +18,7 @@ from ska_sdp_global_sky_model.api.app.crud import (
     alternate_local_sky_model,
     delete_previous_tiles,
     get_local_sky_model,
+    third_local_sky_model,
 )
 from ska_sdp_global_sky_model.api.app.ingest import get_full_catalog, post_process
 from ska_sdp_global_sky_model.api.app.model import Source
@@ -208,6 +209,51 @@ dec:%s, flux_wide:%s, telescope:%s, fov:%s",
         fov,
     )
     local_model = alternate_local_sky_model(
+        db, ra.split(";"), dec.split(";"), flux_wide, telescope, fov
+    )
+    return ORJSONResponse(local_model)
+
+
+@app.get("/third_local_sky_model", response_class=ORJSONResponse)
+async def get_third_local_sky_model_endpoint(
+    ra: str,
+    dec: str,
+    flux_wide: float,
+    telescope: str,
+    fov: float,
+    db: Session = Depends(get_db),
+):
+    """
+    Get the local sky model from a global sky model.
+
+    Args:
+        ra (float): Right ascension of the observation point in degrees.
+        dec (float): Declination of the observation point in degrees.
+        flux_wide (float): Wide-field flux of the observation in Jy.
+        telescope (str): Name of the telescope being used for the observation.
+        fov (float): Field of view of the telescope in arcminutes.
+
+    Returns:
+        dict: A dictionary containing the local sky model information.
+
+        The dictionary includes the following keys:
+            - ra: The right ascension provided as input.
+            - dec: The declination provided as input.
+            - flux_wide: The wide-field flux provided as input.
+            - telescope: The telescope name provided as input.
+            - fov: The field of view provided as input.
+            - local_data: ......
+    """
+    logger.info(
+        "Requesting local sky model with the following parameters: ra:%s, \
+dec:%s, flux_wide:%s, telescope:%s, fov:%s",
+        ra,
+        dec,
+        flux_wide,
+        telescope,
+        fov,
+    )
+    local_model = third_local_sky_model(
         db, ra.split(";"), dec.split(";"), flux_wide, telescope, fov
     )
     return ORJSONResponse(local_model)
