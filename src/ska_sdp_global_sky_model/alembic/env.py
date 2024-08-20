51186ac31ad50028ac17f9d15446bb65ec246ea0
Jacob Maresca <jacob.maresca@gmail.com>
2024-08-20 10:45:59 +0100

    NAL-1125 lint

--- src/ska_sdp_global_sky_model/alembic/env.py
+++ src/ska_sdp_global_sky_model/alembic/env.py
@@ -1,9 +1,13 @@
+# pylint: disable=E1101
+"""
+Alembic migrations scripts.
+"""
 from logging.config import fileConfig
 
-from sqlalchemy import engine_from_config
-from sqlalchemy import pool
-
 from alembic import context
+from sqlalchemy import engine_from_config, pool
+
+from ska_sdp_global_sky_model.api.app.model import Base
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -16,7 +20,7 @@ if config.config_file_name is not None:
 
 # add your model's MetaData object here
 # for 'autogenerate' support
-from ska_sdp_global_sky_model.api.app.model import Base
+
 target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
@@ -63,9 +67,7 @@ def run_migrations_online() -> None:
     )
 
     with connectable.connect() as connection:
-        context.configure(
-            connection=connection, target_metadata=target_metadata
-        )
+        context.configure(connection=connection, target_metadata=target_metadata)
 
         with context.begin_transaction():
             context.run_migrations()
