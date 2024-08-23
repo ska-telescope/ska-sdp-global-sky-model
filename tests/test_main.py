46d411296ea11b1c5777e712dcc5cfb19e1ea5db
Jacob Maresca <jacob.maresca@gmail.com>
2024-08-23 08:36:14 +0100

    NAL-1174 adjust lsm

--- tests/test_main.py
+++ tests/test_main.py
@@ -103,7 +103,7 @@ def test_local_sky_model(myclient):
 
     local_sky_model = myclient.get(
         "/local_sky_model/",
-        params={"ra": 357, "dec": -74, "telescope": "MWA", "flux_wide": 1, "fov": 1},
+        params={"ra": 359, "dec": -74, "telescope": "MWA", "flux_wide": 0, "fov": 1},
     )
 
     assert local_sky_model.status_code == 200
