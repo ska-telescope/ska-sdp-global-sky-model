0920a4927215df15ba21111ed97b3b35b66af15c
Jacob Maresca <jacob.maresca@gmail.com>
2024-10-14 10:21:39 +0100

    NAL-1181 lint

--- src/ska_sdp_global_sky_model/api/app/crud.py
+++ src/ska_sdp_global_sky_model/api/app/crud.py
@@ -61,9 +61,7 @@ def get_precise_local_sky_model(db, ra, dec, fov):
         db.query(Source, narrowband_data, wideband_data)
         .filter(
             or_(
-                Source.Heal_Pix_Position.between(
-                    tile_range.hpx.lower, tile_range.hpx.upper
-                )
+                Source.Heal_Pix_Position.between(tile_range.hpx.lower, tile_range.hpx.upper)
                 for tile_range in healpix_tiles
             )
             .outerjoin(narrowband_data, Source.id == narrowband_data.source)
