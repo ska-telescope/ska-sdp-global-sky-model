863cb74d55ac6e5c07bc2339824ccfeceed7b017
Christian Christelis <cchristelis@ska.ac.za>
2024-11-04 23:04:24 +0200

    NAL-1264 updating tests.

--- tests/test_main.py
+++ tests/test_main.py
@@ -2,44 +2,24 @@
 """
 Basic testing of the API
 """
-
 import pytest
 from fastapi.testclient import TestClient
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 
-from ska_sdp_global_sky_model.api.app.main import Base, app, get_db
-from ska_sdp_global_sky_model.configuration.config import DB_URL
-
-SQLALCHEMY_DATABASE_URL = DB_URL
-
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
-
-TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+from ska_sdp_global_sky_model.api.app.main import app , DataStore , get_ds
+from json import loads
 
 
-def override_get_db():
+TEST_DATASTORE: DataStore = DataStore('tests/datasets')
+
+
+def override_get_ds():
+    """Get the test datastore
     """
-    Create a local testing session.
-    """
-    try:
-        db = TestingSessionLocal()
-        yield db
-    finally:
-        db.close()
+    # TEST_DATASTORE.
+    return TEST_DATASTORE
 
 
-@pytest.fixture()
-def test_db():
-    """
-    Database for test purposes.
-    """
-    Base.metadata.create_all(bind=engine)
-    yield
-    Base.metadata.drop_all(bind=engine)
-
-
-app.dependency_overrides[get_db] = override_get_db
+app.dependency_overrides[get_ds] = override_get_ds
 
 
 @pytest.fixture(scope="module", name="myclient")
@@ -59,52 +39,38 @@ def test_read_main(myclient):
     assert response.status_code == 200
     assert response.json() == {"ping": "live"}
 
-
-def test_upload_rcal(myclient):
-    """Unit test for the /upload_rcal path"""
-    file_path = "tests/data/rcal.csv"
-    # Open the file in binary mode
-    with open(file_path, "rb") as file:
-        # Create a dictionary with the file
-        files = {"file": file}
-
-        # Send a POST request to the FastAPI endpoint
-        response = myclient.post("/upload-rcal/", files=files)
-
-    assert response.status_code == 200
-    assert response.json() == {"message": "RCAL uploaded and ingested successfully"}
+#
+# def test_upload_rcal(myclient):
+#     """Unit test for the /upload_rcal path"""
+#     file_path = "tests/data/rcal.csv"
+#     # Open the file in binary mode
+#     with open(file_path, "rb") as file:
+#         # Create a dictionary with the file
+#         files = {"file": file}
+#
+#         # Send a POST request to the FastAPI endpoint
+#         response = myclient.post("/upload-rcal/", files=files)
+#
+#     assert response.status_code == 200
+#     assert response.json() == {"message": "RCAL uploaded and ingested successfully"}
 
 
 def test_sources(myclient):
     """Unit test for the /local_sky_model path"""
-    file_path = "tests/data/rcal.csv"
-    # Open the file in binary mode
-    with open(file_path, "rb") as file:
-        # Create a dictionary with the file
-        files = {"file": file}
-
-        # Send a POST request to the FastAPI endpoint
-        myclient.post("/upload-rcal/", files=files)
     response = myclient.get("/sources")
     assert response.status_code == 200
-    assert response.json()[0][0] == "J235613-743047"
+    assert len(loads(response.json())) == 228
 
 
 def test_local_sky_model(myclient):
     """Unit test for the /local_sky_model path"""
-    file_path = "tests/data/rcal.csv"
-    # Open the file in binary mode
-    with open(file_path, "rb") as file:
-        # Create a dictionary with the file
-        files = {"file": file}
-
-        # Send a POST request to the FastAPI endpoint
-        myclient.post("/upload-rcal/", files=files)
-
     local_sky_model = myclient.get(
         "/local_sky_model/",
-        params={"ra": 359, "dec": -74, "telescope": "MWA", "flux_wide": 0, "fov": 1},
+        params={"ra": 62, "dec": 15, "telescope": "TEST", "flux_wide": 0, "fov": 0.5},
     )
 
     assert local_sky_model.status_code == 200
-    assert len(local_sky_model.json()) >= 1
+    data = ""
+    for chunk in local_sky_model.iter_text():
+        data += chunk
+    assert len(loads(data)) == 10
