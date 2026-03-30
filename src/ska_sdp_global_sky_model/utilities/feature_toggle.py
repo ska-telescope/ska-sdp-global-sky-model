22592b511f6630750700ea95e58e441d33a455cd
Dominic Schaff <dominic@schaff.dev>
2026-03-30 11:34:46 +0200

    PHX-426 make GSM resource management aware

---
+++ src/ska_sdp_global_sky_model/utilities/feature_toggle.py
@@ -0,0 +1,38 @@
+"""Feature toggle."""
+
+import os
+
+
+class FeatureToggle:
+    """Feature toggle."""
+
+    def __init__(self, name: str, default: bool):
+        """Initialise feature toggle.
+
+        :param name: Name of feature.
+        :param default: Default value for toggle.
+
+        """
+        self._name = name
+        self._default = default
+
+    def set_default(self, default: bool) -> None:
+        """Set feature default toggle value.
+
+        :param default: Default value for toggle.
+
+        """
+        self._default = default
+
+    def is_active(self) -> bool:
+        """Check if feature is active.
+
+        :returns: Toggle value.
+
+        """
+        env_var = str("feature_" + self._name).upper()
+        if env_var in os.environ:
+            value = os.environ.get(env_var) == "1"
+        else:
+            value = self._default
+        return value
