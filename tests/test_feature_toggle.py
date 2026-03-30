9040720d6ea03e04436dbbd04eac4c3a0306b923
Dominic Schaff <dominic@schaff.dev>
2026-03-30 14:08:05 +0200

    PHX-426 fix linting issues

--- tests/test_feature_toggle.py
+++ tests/test_feature_toggle.py
@@ -6,6 +6,9 @@ from ska_sdp_global_sky_model.utilities.feature_toggle import FeatureToggle
 
 
 def test_basic_usage():
+    """Test basic usage of the class"""
+
+    # pylint: disable=protected-access
     feature_toggle = FeatureToggle("MY_FEATURE", False)
 
     assert feature_toggle._key == "FEATURE_MY_FEATURE"
@@ -33,6 +36,7 @@ def test_basic_usage():
     ],
 )
 def test_feature_enabled(monkeypatch, env_val, expected_result):
+    """Test various options for the ENV value"""
     monkeypatch.setenv("FEATURE_MY_FEATURE", env_val)
 
     feature_toggle = FeatureToggle("MY_FEATURE", False)
